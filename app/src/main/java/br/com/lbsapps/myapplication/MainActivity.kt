34ce6eb71a1018b2fe1371c1caf2cf3dbb002d1c
Leandro Bastos <android@leandrobastos.dev>
2026-03-26 17:35:19 -0300

    fixing state

--- app/src/main/java/br/com/lbsapps/myapplication/MainActivity.kt
+++ app/src/main/java/br/com/lbsapps/myapplication/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -25,6 +26,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MyApplicationTheme {
+                val state = remember { loginViewModel.loginState }
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     if (loginViewModel.loginState.value.loggedIn) {
                         Column(modifier = Modifier.padding(80.dp)) {
--- app/src/main/java/br/com/lbsapps/myapplication/ui/view/LoginView.kt
+++ app/src/main/java/br/com/lbsapps/myapplication/ui/view/LoginView.kt
@@ -7,6 +7,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import br.com.lbsapps.myapplication.ui.viewmodel.LoginViewModel
@@ -15,7 +16,7 @@ import br.com.lbsapps.myapplication.ui.viewmodel.LoginViewModel
 fun LoginView(
     viewModel: LoginViewModel
 ) {
-    val state = viewModel.loginState
+    val state = remember { viewModel.loginState }
     Column(
         modifier = Modifier.padding(80.dp)
     ) {
